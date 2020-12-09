2372aba43cfa65b3ae49d65fc225e98015e576db
cdeck3r <christian.decker@reutlingen-university.de>
2020-12-09 07:59:05 +0100

    First step to pull out hardcoded paths

--- src/raspi-autosetup/install_camnode.sh
+++ src/raspi-autosetup/install_camnode.sh
@@ -17,6 +17,10 @@ USER=pi
 USER_HOME="/home/${USER}"
 IMG_DIR="${USER_HOME}/images"
 REPO_DIR="/boot/autosetup/3DScanner"
+HOMIE_NODES_DIR="${REPO_DIR}/src/homie-nodes"
+HOMIE_CAMNODE_DIR="${HOMIE_NODES_DIR}/homie-camnode"
+HOMIE_CAMNODE_USER_DIR="${USER_HOME}/$(basename ${HOMIE_CAMNODE_DIR})"
+SERVICE_INSTALL_SCRIPT="${HOMIE_NODES_DIR}/install_homie_service.sh"
 
 # ignore wrong date
 apt-get update -o Acquire::Check-Valid-Until=false -o Acquire::Check-Date=false
@@ -40,11 +44,11 @@ pip3 install --force-reinstall picamera
 # 2. mkdir IMG_DIR
 # 3. enable the service start at boot
 # 4. install service
-rm -rf "${USER_HOME}/homie-camnode" # cleanup
-su -c "cp -r ${REPO_DIR}/src/homie-nodes/homie-camnode ${USER_HOME}" "${USER}"
+rm -rf "${HOMIE_CAMNODE_USER_DIR}" # cleanup
+su -c "cp -r ${HOMIE_CAMNODE_DIR} ${USER_HOME}" "${USER}"
 su -c "mkdir -p ${IMG_DIR}" "${USER}"
 
 # enable the service start at each Raspi boot-up for the user ${USER}
 loginctl enable-linger "${USER}" || { echo "Error ignored: $?"; }
-chmod 755 /boot/autosetup/3DScanner/src/homie-nodes/install_homie_service.sh
+chmod 755 "${SERVICE_INSTALL_SCRIPT}"
 su -c 'XDG_RUNTIME_DIR=/run/user/$(id -u) /boot/autosetup/3DScanner/src/homie-nodes/install_homie_service.sh' "${USER}"
