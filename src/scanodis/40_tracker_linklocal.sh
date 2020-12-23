517c9e9d01d2990a34de5a2b136d9e7cfdc4c88f
cdeck3r <christian.decker@reutlingen-university.de>
2020-12-23 23:27:05 +0100

    Add avahi commands

--- src/scanodis/40_tracker_linklocal.sh
+++ src/scanodis/40_tracker_linklocal.sh
@@ -15,13 +15,17 @@
 
 publish_to_tracker() {
     local TRACKER
+    local TMPFILE
+    
+    TMPFILE=$(mktemp)
 
     # check for installed program
     # Source: https://stackoverflow.com/a/677212
     command -v "avahi-browse" >/dev/null 2>&1 || { log_echo "ERROR" "I require avahi-browse, but it's not installed.  Abort."; return 1; }
 
     # run avahi-browse
-    # avahi-browse -atp | grep camnode | grep -i ipv4 | grep -i ssh 
+    # avahi-browse -atr | grep hostname | grep camnode | | cut -d' ' -f6 | cut -d'[' -f2 | cut -d']' -f1 | uniq > "${TMPFILE}" 
+    # avahi-resolve -4 -n ${h} 
     
     return 0
 }
