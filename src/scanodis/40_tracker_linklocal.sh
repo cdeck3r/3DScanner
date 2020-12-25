549c6d62060ca55a014a8d02f942ac9c27686061
cdeck3r <christian.decker@reutlingen-university.de>
2020-12-26 00:18:32 +0100

    Add link-local tracker

--- src/scanodis/40_tracker_linklocal.sh
+++ src/scanodis/40_tracker_linklocal.sh
@@ -16,16 +16,28 @@
 publish_to_tracker() {
     local TRACKER
     local TMPFILE
+    local NODELIST
     
     TMPFILE=$(mktemp)
+    NODELIST="${LOG_DIR}/nodelist.log"
 
     # check for installed program
     # Source: https://stackoverflow.com/a/677212
     command -v "avahi-browse" >/dev/null 2>&1 || { log_echo "ERROR" "I require avahi-browse, but it's not installed.  Abort."; return 1; }
+    command -v "avahi-resolve" >/dev/null 2>&1 || { log_echo "ERROR" "I require avahi-resolve, but it's not installed.  Abort."; return 1; }
+
+    log_echo "INFO" "Tracker link-local"
 
     # run avahi-browse
-    # avahi-browse -atr | grep hostname | grep camnode | | cut -d' ' -f6 | cut -d'[' -f2 | cut -d']' -f1 | uniq > "${TMPFILE}" 
-    # avahi-resolve -4 -n ${h} 
+    avahi-browse -atr | grep hostname | grep camnode | tr '[:space:]' '\n' | grep local | sort | uniq | sed 's/\[\(.\+\)\]/\1/g' > "${TMPFILE}"
+    # resolve all found hosts and log them
+    while IFS="" read -r h || [ -n "$h" ]
+    do
+        avahi-resolve -4 -n "${h}" >> "${NODELIST}"
+    done < "${TMPFILE}"
+    
+    # cleanup
+    rm -rf "${TMPFILE}"
     
     return 0
 }
